794614ecde7503f4f1b9c1cea1ed0ca2c6a21dae
programmingishardlol <99515218+programmingishardlol@users.noreply.github.com>
2022-12-21 19:35:48 -0800

    Create square.py

---
+++ square.py
@@ -0,0 +1,42 @@
+class Square:
+
+    ALPHACOLS = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'}
+
+    def __init__(self, row, col, piece=None):
+        self.row = row
+        self.col = col
+        self.piece = piece
+        self.alphacol = self.ALPHACOLS[col]
+
+    def __eq__(self, object):
+        return self.row == object.row and self.col == object.col
+
+    def has_piece(self):
+        return self.piece != None
+
+    def isempty(self):
+        return not self.has_piece()
+
+    def has_team_piece(self, color):
+        return self.has_piece() and self.piece.color == color
+
+    def has_rival_piece(self, color):
+        return self.has_piece() and self.piece.color != color
+
+    def isempty_or_rival(self, color):
+        return self.isempty() or self.has_rival_piece(color)
+
+    # can call this method without needed to create a Square object
+    # s = Square() no need, just Square.in_range()
+    # access with the class, not object
+    @staticmethod
+    def in_range(*args):
+        for arg in args:
+            if arg < 0 or arg > 7:
+                return False
+        return True
+
+    @staticmethod
+    def get_alphacol(col):
+        ALPHACOLS = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'}
+        return ALPHACOLS[col]
